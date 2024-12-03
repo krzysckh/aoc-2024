50188abde5bf8a7ab0485a961ee5048a8573ca3d
krzysckh <kpm@krzysckh.org>
2024-12-03 12:19:14 +0100

    shorter code for day 3

--- 03/3.pl
+++ 03/3.pl
@@ -7,31 +7,27 @@ use Data::Printer;
 use List::MoreUtils ':all';
 use Switch::Back;
 
-my $data = read_file('input');
+my @data = read_file('input') =~ /(?:don\'t\(\))|(?:do\(\))|(?:mul\((?:\d{1,3}),(?:\d{1,3})\))/g;
 
 sub p1 {
-  my @muls = $data =~ /mul\((\d{1,3}),(\d{1,3})\)/g;
-  my $sum = 0;
+  my $sum;
 
-  my $it = natatime(2, @muls);
-  while (($a, $b) = $it->()) {
-    $sum += ($a * $b);
+  for (@data) {
+    $sum += $1 * $2 if /mul\((\d+),(\d+)\)/
   }
 
-  say "p1: $sum";
+  say "p1: $sum"
 }
 
 sub p2 {
-  my ($sum, $dont) = (0, 0);
-  for ($data =~ /(?:don\'t\(\))|(?:do\(\))|(?:mul\((?:\d{1,3}),(?:\d{1,3})\))/g) {
-    given ($_) {
-      when (/mul/) { do { my @l = /mul\((\d+),(\d+)\)/; $sum += $l[0]*$l[1] } unless $dont };
-      when (/don/) { $dont = 1 }
-      default      { $dont = 0 }
-    }
+  my ($sum, $dont);
+  for (@data) {
+    when (/mul\((\d+),(\d+)\)/) { $sum += $1 * $2 unless $dont }
+    when (/don/)                { $dont = 1 }
+    default                     { $dont = 0 }
   }
 
-  say "p2: $sum";
+  say "p2: $sum"
 }
 
 p1;
